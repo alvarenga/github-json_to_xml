3c025363f61352b5eaf7d54e03136623e35c417f
edson_alvarenga <alvarenga.edson@gmail.com>
2015-02-23 21:53:48 -0300

    Ajustes Gerais

---
+++ github-json_to_xml/conv.py
@@ -0,0 +1,14 @@
+def conv(user):
+    import requests
+    import json
+    import xmltodict
+
+    url = 'https://api.github.com/users/' + user
+    s = requests.get(url)
+
+    # Converter json para dict
+    x = {}
+    x['wg'] = json.loads(s.text)
+    y = xmltodict.unparse(x, pretty=True)
+    # print(y)
+    return y
