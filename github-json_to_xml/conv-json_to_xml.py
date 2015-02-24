0299129b35a5b52e3c6aff00e52c6b93da124957
edson_alvarenga <alvarenga.edson@gmail.com>
2015-02-23 21:12:15 -0300

    Ajustes Gerais

--- github-json_to_xml/conv-json_to_xml.py
+++ github-json_to_xml/conv-json_to_xml.py
@@ -2,14 +2,10 @@ from pip._vendor import requests
 import json
 import xmltodict
 
-s = requests.get('https://api.github.com/users/alvarenga')
-# print (json.dumps(res_json.text,indent=1))
+s = requests.get('https://api.github.com/users/renzon')
 
-print (s.text)
 # Converter json para dict
-x = json.loads(s.text)
-
-print (x)
-
-# print(xmltodict.unparse({'nome': 'Renzo'}))
-print(xmltodict.unparse(x))
+x = {}
+x['wg'] = json.loads(s.text)
+y = xmltodict.unparse(x, pretty=True)
+print(y)
